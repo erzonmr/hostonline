de0b947aa59a6b89483726ddcf70d7b72e4da2d4
Erzon Moya <41084404+erzonmr@users.noreply.github.com>
2024-09-03 20:23:51 -0500

    Update Script_LC_Lista-Artistas.js

--- Script_LC_Lista-Artistas.js
+++ Script_LC_Lista-Artistas.js
@@ -1,8 +1,8 @@
-   document.querySelectorAll('.tag-link').forEach(function(link) {
+    document.querySelectorAll('.tag-link').forEach(function(link) {
         link.addEventListener('click', function(event) {
             event.preventDefault(); // Prevenir la redirección inmediata
             const tag = link.querySelector('b').textContent.trim(); // Obtener el texto dentro de <b>
             const newHref = `/p/results.html?tag=${encodeURIComponent(tag)}`; // Construir la nueva URL con el tag
-            window.location.href = newHref; // Redirigir a la nueva URL
+            window.open(newHref, '_blank'); // Abrir la nueva URL en una nueva pestaña o ventana
         });
     });
