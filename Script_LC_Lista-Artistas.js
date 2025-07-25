7f56b4794c8a7e936bbe1485517f6b0d27a8f959
Erzon Moya <41084404+erzonmr@users.noreply.github.com>
2025-07-25 18:08:35 -0500

    Update Script_LC_Lista-Artistas.js
    Se modifica script al generar la url por actualización del template de Blogger

--- Script_LC_Lista-Artistas.js
+++ Script_LC_Lista-Artistas.js
@@ -1,7 +1,7 @@
-    document.querySelectorAll('.tag-link').forEach(function(link) {
+    document.querySelectorAll('#myArtistListContainer .tag-link').forEach(function(link) {
         link.addEventListener('click', function(event) {
             event.preventDefault(); // Prevenir la redirección inmediata
-            const tag = link.querySelector('b').textContent.trim(); // Obtener el texto dentro de <b>
+            const tag = link.textContent.trim(); // Obtener el texto directamente del enlace
             const newHref = `/p/results.html?tag=${encodeURIComponent(tag)}`; // Construir la nueva URL con el tag
             window.open(newHref, '_blank'); // Abrir la nueva URL en una nueva pestaña o ventana
         });
